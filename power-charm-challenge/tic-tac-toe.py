1d0be740f985e089d3d81e549780bb0cc14d6b3f
Rancencole (Sloate) <kjsloate@ualr.edu>
2025-05-13 18:50:23 -0500

    Refactored and renamed varibles for clarity

--- power-charm-challenge/tic-tac-toe.py
+++ power-charm-challenge/tic-tac-toe.py
@@ -1,47 +1,47 @@
-def p(b):
- for r in b:
+def print_board(board):
+ for r in board:
   print(" | ".join(r))
   print("-"*5)
  
  
-def c_w(b,p):
+def check_win(board, player):
  for i in range(3):
-  if all(b[i][j]==p for j in range(3)) or all(b[j][i]==p for j in range(3)):
+  if all(board[i][j] == player for j in range(3)) or all(board[j][i] == player for j in range(3)):
    return True
- if all(b[i][i]==p for i in range(3)) or all(b[i][2-i]==p for i in range(3)):
+ if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
   return True
  return False
  
  
-def f(b):
- return all(c!=" " for r in b for c in r)
+def board_full(board):
+ return all(c !=" " for r in board for c in r)
  
  
-def t():
- b=[[" "for _ in range(3)]for _ in range(3)]
+def play_game():
+ board=[[" "for _ in range(3)]for _ in range(3)]
  p=["X","O"]
  print("Tic-Tac-Toe Game")
- p(b)
- for t in range(9):
-  pl=p[t%2]
+ p(board)
+ for turn in range(9):
+  current_player =p[turn%2]
   while 1:
    try:
-    r,c=map(int,input(f"P {pl}, row col (0-2): ").split())
-    if b[r][c]==" ":
-     b[r][c]=pl
+    r,c=map(int,input(f"P {current_player}, row col (0-2): ").split())
+    if board[r][c]==" ":
+     board[r][c]=current_player
      break
     else:
      print("Nope. Again.")
    except:
     print("Wrong. 0-2 pls.")
-  p(b)
-  if c_w(b,pl):
-   print(f"P {pl} wins!")
+  p(board)
+  if check_win(board, current_player):
+   print(f"P {current_player} wins!")
    return
-  if f(b):
+  if board_full(board):
    print("Draw!")
    return
  print("Draw!")
  
  
-t() 
+play_game()
